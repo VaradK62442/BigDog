dd35e38ef6901095a8548957ec9ad7ebc42c3cb8
VaradK62442 <varadkulk123@gmail.com>
2024-06-07 21:42:22 +0100

    register models in appropriate files

--- Noteport/quiz/admin.py
+++ Noteport/quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# from quiz.models import StandardQuiz, MultipleChoiceQuestion, TrueFalseQuestion
+
+# admin.site.register(StandardQuiz)
+# admin.site.register(MultipleChoiceQuestion)
+# admin.site.register(TrueFalseQuestion)
