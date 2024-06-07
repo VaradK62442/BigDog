27930599f6c92e332b0f1998b28fab4c369bd5bc
VaradK62442 <varadkulk123@gmail.com>
2024-06-07 22:09:33 +0100

    login, registering, logout fully working

--- Noteport/account/urls.py
+++ Noteport/account/urls.py
@@ -5,4 +5,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- Noteport/account/views.py
+++ Noteport/account/views.py
@@ -1,4 +1,83 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
+
+from account.forms import UserForm, UserProfileForm
+from account.models import UserProfile
+
+
+@login_required
 def index(request):
     return render(request, 'account/index.html')
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('account:index'))
+            else:
+                return redirect(reverse('account:login'))
+            
+        else:
+            return redirect(reverse('account:login'))
+    
+    else:
+        return render(request, 'account/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('account:index'))
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.profilePicture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(
+        request,
+        'account/register.html',
+        context={
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': registered
+        }
+    )
